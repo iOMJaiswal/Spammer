ca5377f482729ed91409c45b1786f910d114bf88
OM JAISWAL <jaiswalom268@gmail.com>
2023-01-06 19:44:13 +0530

    Debug = True

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gqi*vrtjls(_x5@7xcs0%p@8wfo-r^3ggmkvb9u%mv9h+u%-$9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [".vercel.app", ".now.sh"]
 
